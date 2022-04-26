9a69f80c306b0aed9df2614eae18429f38015a01
filippomrusso <filipmau.russo@hotmail.it>
2022-04-26 21:56:16 +0200

    tutto modified

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Saluta{
+
+        string Stringa = " Ciao! Questa è una stringa";
+
+        short corto   = 1;
+        int intero {get;} = 125;
+        long lungo    = 121234245324;
+        double doppio = 34.321;
+
+        bool interruttore = false;
+        bool? nullable    = null;
+    
+        struct add{
+
+        }
+        
+    static void Main(string [] args){
+
+    
+    }
+
+}
